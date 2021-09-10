c0203b511ae02c9964b66b460cb872db70ae42a9
Anton Schmidt <schmidt.anton2@gmail.com>
2021-09-10 16:24:14 +0200

    created tour class. Still missing image, will do later

---
+++ tourplanner/Models/Tour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tourplanner.Models
+{
+    public class Tour
+    {
+        public int tour_ID { get; set; }
+        public string tour_Name { get; set; }
+        public string tour_Description { get; set; }
+        public float tour_Distance { get; set; }
+    }
+}
