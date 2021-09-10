7e9b41343515d343a4c5656fd3e728d48032c7b6
Anton Schmidt <schmidt.anton2@gmail.com>
2021-09-10 17:28:29 +0200

    Added project files for Data access layer and moved Models to Model Project. Created tourmapper for object mapping of database values

---
+++ tourplanner_DAL/mapper_Base.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tourplanner_DAL
+{
+    abstract class mapper_Base<T>
+    {
+        protected abstract T Map(IDataRecord record);
+        public Collection<T> MapAll(IDataReader reader)
+        {
+            Collection<T> collection = new Collection<T>();
+            while (reader.Read())
+            {
+                try
+                {
+                    collection.Add(Map(reader));
+                }
+                catch
+                {
+                    throw;
+                    // NOTE:  
+                    // Source: https://www.c-sharpcorner.com/article/an-elegant-C-Sharp-data-access-layer-using-the-template-pattern-a/
+                    // consider handling exeption here instead of re-throwing  
+                    // if graceful recovery can be accomplished  
+                }
+            }
+            return collection;
+        }
+    }
+
+}
---
+++ tourplanner_DAL/tourmapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tourplanner.Models;
+
+namespace tourplanner_DAL
+{
+    class tourmapper: mapper_Base<Tour>
+    {
+        protected override Tour Map(IDataRecord record)
+        {
+            try
+            {
+                Tour t = new Tour();
+                t.tour_ID = (DBNull.Value == record["tour_ID"]) ? 0 : (int)record["tour_ID"];
+                t.tour_Name = (DBNull.Value == record["tour_Name"]) ? string.Empty : (string)record["tour_Name"];
+                t.tour_Description = (DBNull.Value == record["tour_Description"]) ? string.Empty : (string)record["tour_Description"];
+                t.tour_Distance = (DBNull.Value == record["tour_Distance"]) ? 0 : (float)record["tour_Distance"];
+                return t;
+            }
+            catch
+            {
+                throw;
+                // NOTE:
+                // Source: https://www.c-sharpcorner.com/article/an-elegant-C-Sharp-data-access-layer-using-the-template-pattern-a/
+                // consider handling exeption here instead of re-throwing  
+                // if graceful recovery can be accomplished  
+            }
+        }
+    }
+}
