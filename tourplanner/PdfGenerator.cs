29758c4c6ad6991c75c0e82dd08548b180484a23
Anton <schmidt.anton2@gmail.com>
2021-09-18 17:36:15 +0200

    added dummy Functions to PdfGenerator Class

--- tourplanner/PdfGenerator.cs
+++ tourplanner/PdfGenerator.cs
@@ -4,10 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using iText;
+using tourplanner.Models;
+using System.Configuration;
+using iText.Layout;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 
 namespace tourplanner
 {
     class PdfGenerator
     {
+        private string filePath = ConfigurationManager.AppSettings.Get("filepath") + "Reports\\";
+
+        public void CreateReport(Tour t)
+        {
+            PdfWriter writer = new PdfWriter(filePath + "Reportdemo.pdf");
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            Paragraph header = new Paragraph("HEADER")
+               .SetTextAlignment(TextAlignment.CENTER)
+               .SetFontSize(20);
+
+            document.Add(header);
+            document.Close();
+        }
+        public void CreateSummary(List<Tour> tours)
+        {
+            PdfWriter writer = new PdfWriter(filePath + "Summarydemo.pdf");
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            Paragraph header = new Paragraph("HEADER")
+               .SetTextAlignment(TextAlignment.CENTER)
+               .SetFontSize(20);
+
+            document.Add(header);
+            document.Close();
+        }
+
     }
 }
